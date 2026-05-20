b353705de1c159314e6e47822bb83508f5038169
Jack <jackaklassen.jak@gmail.com>
2026-05-20 09:30:35 -0700

    added load_rom

--- ui/gui.py
+++ ui/gui.py
@@ -8,10 +8,10 @@ class GUI:
     def __init__(self):
         # Initialize Pygame
         pygame.init()
-
+        WHITE = (255, 255, 255)
         # Set up the game window
         screen = pygame.display.set_mode((64*10, 32 * 10))
-        pygame.display.set_caption("Hello Pygame")
+        pygame.display.set_caption("Chip-8")
 
         # Game loop
         running = True
@@ -19,6 +19,12 @@ class GUI:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+
+            pygame.draw.rect(screen, WHITE, (0, 0, 10, 10)) #draw pixels with this.
+            
+            #x and y on emulator is just a line 64 to 32
+
+            pygame.display.flip()
 
         # Quit Pygame
         pygame.quit()
