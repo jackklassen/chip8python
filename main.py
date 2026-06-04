c93024565a65c480daa79236909aa2058855711c
Jack <Jklass00@students.cs.ubc.ca>
2026-06-03 20:55:07 -0700

    added test framework

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import sys
 from src import chip8
 
-
-
-
 def main():
     args = sys.argv[1:]  # Exclude the script name
     chip8_main = chip8.Chip8(args)
@@ -14,7 +11,6 @@ def main():
 #def main():
  #   chip8_main = chip8.Chip8("2-ibm-logo.ch8")
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
    main()
 
